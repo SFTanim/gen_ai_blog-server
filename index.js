fb11464eca9a6d1204c14516edcc1fcc63ee2493
Sabid Farhan <sfttanim@gmail.com>
2024-12-25 21:40:24 +0600

    blog post like and dislike implemenation done

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash", // Specify the model name
 });
+
 const generationConfig = {
     temperature: 1,
     topP: 0.95,
@@ -70,7 +71,6 @@ app.post('/api/suggest', async (req, res) => {
         }
 
         const generatedText = result.response.text();
-        console.log("Generated Text:", generatedText);
 
         if (!generatedText) {
             return res.status(500).json({ error: "Failed to generate text." });
@@ -95,7 +95,6 @@ app.post('/api/suggest', async (req, res) => {
         // Send the extracted title, subtitle, and description
         res.json({ title, subtitle, description });
     } catch (error) {
-        console.error("Error with Google Generative AI:", error.message || error);
         res.status(500).json({ error: "Error generating text from the AI model." });
     }
 });
@@ -106,9 +105,6 @@ app.post('/api/suggest', async (req, res) => {
 // Ping MongoDB
 async function run() {
     try {
-        // await client.connect();
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const blogCollection = client.db("GenAiBlog").collection("blogs")
@@ -144,7 +140,6 @@ async function run() {
 
         app.post("/blogs", async (req, res) => {
             const data = req.body;
-            console.log(data);
             const result = await blogCollection.insertOne(data)
             res.send(result)
         })
@@ -157,6 +152,75 @@ async function run() {
         })
 
 
+        // Blog Like
+        app.post("/blogLike/:email/:id", async (req, res) => {
+            const userEmail = req.params.email;
+            const blogId = req.params.id
+            const blogFilter = { _id: new ObjectId(blogId) }
+            const blog = await blogCollection.findOne(blogFilter)
+            const options = { upsert: true }
+
+            if (blog.dislike.includes(userEmail) && !blog.like.includes(userEmail)) {
+                updateDoc = {
+                    $push: { like: userEmail },
+                    $pull: { dislike: userEmail }
+                };
+            }
+            else if (blog.dislike.includes(userEmail) && blog.like.includes(userEmail)) {
+                updateDoc = {
+                    $pull: { dislike: userEmail, like: userEmail }
+                };
+            }
+            else if (!blog.dislike.includes(userEmail) && !blog.like.includes(userEmail)) {
+                updateDoc = {
+                    $push: { like: userEmail },
+                };
+            }
+            else if (blog.like.includes(userEmail)) {
+                updateDoc = {
+                    $pull: { like: userEmail },
+                };
+            }
+
+            const resultNew = await blogCollection.updateOne(blogFilter, updateDoc, options)
+            res.send(resultNew);
+        })
+
+        // Blog Dislike
+        app.post("/blogDislike/:email/:id", async (req, res) => {
+            const userEmail = req.params.email;
+            const blogId = req.params.id
+            const blogFilter = { _id: new ObjectId(blogId) }
+            const blog = await blogCollection.findOne(blogFilter)
+            const options = { upsert: true }
+
+            if (blog.dislike.includes(userEmail) && !blog.like.includes(userEmail)) {
+                updateDoc = {
+                    $pull: { dislike: userEmail }
+                };
+            }
+            else if (blog.dislike.includes(userEmail) && blog.like.includes(userEmail)) {
+                updateDoc = {
+                    $pull: { dislike: userEmail, like: userEmail }
+                };
+            }
+            else if (!blog.dislike.includes(userEmail) && !blog.like.includes(userEmail)) {
+                updateDoc = {
+                    $push: { dislike: userEmail },
+                };
+            }
+            else if (!blog.dislike.includes(userEmail) && blog.like.includes(userEmail)) {
+                updateDoc = {
+                    $pull: { like: userEmail },
+                    $push: { dislike: userEmail },
+                };
+            }
+
+            const resultNew = await blogCollection.updateOne(blogFilter, updateDoc, options)
+            res.send(resultNew);
+        })
+
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -165,6 +229,7 @@ async function run() {
 run().catch(console.dir);
 
 // Default route
+
 app.get("/", (req, res) => {
     res.send("GenAiBlog Server Is Running....");
 });
